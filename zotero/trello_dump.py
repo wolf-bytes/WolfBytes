7a3e4510f55ae41bdc5ff3efa6d1db7257c70236
Mark Coletti <mcoletti@gmail.com>
2018-08-06 19:44:11 -0400

    Added start of trello key.

---
+++ zotero/trello_dump.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+    trello test bed
+"""
+
+
+from trello import TrelloClient
+
+client = TrelloClient(
+    api_key='7f436f4b51aef52fcc0d0255112bc279',
+    api_secret='your-secret',
+    token='428181ca62954c133da2cc51341a4a259b190a49f9b9c5752e78e99685c60a0f',
+    token_secret='e882f734ec015345211ecdaaa0b96009e94125e93ea62cc08baa961082c0156f'
+)
+
+
