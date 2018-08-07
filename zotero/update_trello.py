98058c077823810dc3efe6bfe99983b82ad8c6c5
Mark Coletti <mcoletti@gmail.com>
2018-08-06 23:31:35 -0400

    Trying to repair mail invocation

--- zotero/update_trello.py
+++ zotero/update_trello.py
@@ -82,7 +82,7 @@ def emit_email_lines(untagged_items):
         print('Processing', ''.join(item['title']))
 
         subject = '"#yellow #red ' + ''.join(item['title']) + '"'
-        output = subprocess.check_output(['/bin/echo', '"Add description"', '|', '/usr/bin/mail', '-s', subject, TRELLO_EMAIL])
+        output = subprocess.check_output(['/bin/echo', 'Nada', '|', '/usr/bin/mail', '-s', subject, TRELLO_EMAIL], shell=True)
 
         print(output)
 
