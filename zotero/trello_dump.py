548dff684378d4a101ad99bf7544045529c2ba65
Mark Coletti <mcoletti@gmail.com>
2019-01-31 20:48:52 -0500

    Removed API key.

--- zotero/trello_dump.py
+++ zotero/trello_dump.py
@@ -7,10 +7,7 @@
 from trello import TrelloClient
 
 client = TrelloClient(
-    api_key='7f436f4b51aef52fcc0d0255112bc279',
-    api_secret='your-secret',
-    token='428181ca62954c133da2cc51341a4a259b190a49f9b9c5752e78e99685c60a0f',
-    token_secret='e882f734ec015345211ecdaaa0b96009e94125e93ea62cc08baa961082c0156f'
+# removed, MAC, 1/31/2019.  Encrypted and saved key elsewhere.
 )
 
 
